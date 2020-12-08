edc9b787046a28f24ccde90c1d751e926a44c2e1
Makwale <makwale.em@gmail.com>
2020-12-08 15:53:43 +0200

    Thirthin commit

--- src/app/tab/tab.page.ts
+++ src/app/tab/tab.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-tab',
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabPage implements OnInit {
 	appear = false;
-  constructor() { }
+  constructor(public cartService: CartService) { }
 
   ngOnInit() {
   }
-
 }
